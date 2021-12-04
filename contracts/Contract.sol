67e1af5f0d07d0781291961448cdcbcac7442c59
turinglabsorg <s.cataudo@turinglabs.org>
2021-12-04 14:44:09 +0100

    other fix

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Contract is Ownable {
-    /*
-        This method will add or remove minting roles.
-    */
     function hello(string memory world) public pure returns (string memory) {
         return world;
     }
